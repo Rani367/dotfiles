7f0ee2b7fa4a34b58ebb8832c12090edbf26b9f9
Rani367 <rani2011367@gmail.com>
2025-12-03 16:10:49 +0200

    configure completion options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -38,3 +38,5 @@ local mode_map = {
   n = "-- NORMAL --", i = "-- INSERT --", v = "-- VISUAL --", V = "-- VISUAL LINE --", ["\22"] = "-- VISUAL BLOCK --",
   c = "-- COMMAND --", R = "-- REPLACE --", t = "-- TERMINAL --", s = "-- SELECT --", S = "-- SELECT LINE --",
 }
+function _G.statusline_mode()
+  local mode = vim.api.nvim_get_mode().mode:sub(1, 1)
