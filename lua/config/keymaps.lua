e9e9943aaf9ed5afddd317cdab9564314fcdcecc
Rani367 <rani2011367@gmail.com>
2025-12-08 16:14:07 +0200

    add search highlight clear

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,6 @@ local map = vim.keymap.set
 map("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
+
+map("n", "<leader>r", function()
+  require("util.runner").run_current_file()
