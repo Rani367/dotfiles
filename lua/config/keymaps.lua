89caa5859874128396c96d91057421d97d607a7a
Rani367 <rani2011367@gmail.com>
2025-12-08 11:47:20 +0200

    add save and quit keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 local map = vim.keymap.set
 
 map("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
