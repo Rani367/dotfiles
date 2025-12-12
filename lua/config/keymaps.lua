57cfd04b0ab6f8e974ef73c818b323ccc529f8e7
Rani367 <rani2011367@gmail.com>
2025-12-12 04:43:07 +0200

    update key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -54,3 +54,6 @@ map("x", "p", [["_dP]], { desc = "Paste without yanking" })
 -- native 0.11 LSP: grn (rename), gra (code_action), grr (references), gri (implementation), gO (document_symbol)
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+map("n", "<leader>cf", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format buffer" })
+
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line diagnostics" })
