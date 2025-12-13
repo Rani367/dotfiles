e039c35a58255f93674b4e893acf6ba92d146874
Rani367 <rani2011367@gmail.com>
2025-12-13 20:44:13 +0200

    implement session-aware startup picker

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -20,3 +20,5 @@ autocmd("BufReadPost", {
   group = augroup("last_position", { clear = true }),
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
