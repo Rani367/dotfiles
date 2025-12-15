46f3cd95a02738fd5c8972aa2270aa84ffc97b26
Rani367 <rani2011367@gmail.com>
2025-12-15 08:18:31 +0200

    update autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -41,3 +41,6 @@ autocmd("FileType", {
   pattern = { "python", "rust" },
   callback = function()
     vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
