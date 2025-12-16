cbc2f247ea0a1c785c5c2a885a6842b7fca81133
Rani367 <rani2011367@gmail.com>
2025-12-17 00:19:37 +0200

    update autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -65,3 +65,6 @@ autocmd("VimEnter", {
         local has_real_buffer = false
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
           if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= "" then
+            has_real_buffer = true
+            break
+          end
