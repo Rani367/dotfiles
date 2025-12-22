80b9411ee17031c748a9512c97a6d35b7492a44e
Rani367 <rani2011367@gmail.com>
2025-12-22 09:16:30 +0200

    finalize treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,3 +5,6 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+    require("nvim-treesitter").install({
