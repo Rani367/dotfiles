3c90a1f0d967e2e1bfcd2099a7f124d017aee586
Rani367 <rani2011367@gmail.com>
2025-12-28 16:27:20 +0200

    configure diagnostic virtual text

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,3 +33,6 @@ return {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
       "saghen/blink.cmp",
+    },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
