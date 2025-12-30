fbd1366ddcff06513c7312e59dec7a511312a1e7
Rani367 <rani2011367@gmail.com>
2025-12-30 08:28:26 +0200

    update LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,3 +57,6 @@ return {
           "clangd",
           "--background-index",
           "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+        },
