bbc865936ed32090fadb78c33907cea196043b5b
Rani367 <rani2011367@gmail.com>
2026-01-01 13:49:54 +0200

    update LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,3 +89,6 @@ return {
             [vim.diagnostic.severity.ERROR] = " ",
             [vim.diagnostic.severity.WARN] = " ",
             [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
+        },
