67967898af7ee2e0801be906ee6dc00b850d9269
Rani367 <rani2011367@gmail.com>
2026-01-05 20:06:03 +0200

    update roslyn config

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -14,3 +14,5 @@ return {
     vim.lsp.config("roslyn", {
       capabilities = require("blink.cmp").get_lsp_capabilities(),
       settings = {
+        ["csharp|inlay_hints"] = {
+          csharp_enable_inlay_hints_for_implicit_object_creation = true,
