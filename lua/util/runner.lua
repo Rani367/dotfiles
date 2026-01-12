5d2c089b78a856321f12364122de2693540da089
Rani367 <rani2011367@gmail.com>
2026-01-12 16:37:14 +0200

    implement temp file generation

--- lua/util/runner.lua
+++ lua/util/runner.lua
@@ -3,3 +3,6 @@ local M = {}
 local function esc(path) return vim.fn.shellescape(path, 1) end
 local function has(cmd) return vim.fn.executable(cmd) == 1 end
 local function find_exe(candidates)
+  for _, exe in ipairs(candidates) do
+    if has(exe) then return exe end
+  end
