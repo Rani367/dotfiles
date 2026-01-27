dda4acb94d3b30afa567b2341bfca44a84a3ece9
Rani367 <rani2011367@gmail.com>
2026-01-27 21:40:02 +0200

    fix wezterm ligature disable: use table syntax and global harfbuzz_features

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,11 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font Mono", { harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
+config.font = wezterm.font({
+	family = "FiraCode Nerd Font Mono",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 27
 config.enable_tab_bar = false
 config.window_padding = {
