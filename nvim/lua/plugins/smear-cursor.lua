5b64f7fc95b8e48f0957f6e51d98faf44ef806ba
Rani367 <rani2011367@gmail.com>
2026-01-30 22:20:51 +0200

    add smear-cursor.nvim for smooth cursor animation on jumps

---
+++ nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  lazy = false,
+  opts = {
+    stiffness = 0.4,
+    trailing_stiffness = 0.5,
+    distance_stop_animating = 0.2,
+    time_interval = 17,
+    min_horizontal_distance_smear = 9999,
+    min_vertical_distance_smear = 2,
+  },
+}
