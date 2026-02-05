602ea9fe983973204103fe6c34beced697741448
Rani367 <rani2011367@gmail.com>
2026-02-05 22:01:34 +0200

    Remove code runner - just use a separate terminal

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,14 +4,6 @@ map("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
 
-map("n", "<leader>r", function()
-  require("util.runner").run_current_file()
-end, { desc = "Run current file" })
-
-map("t", "<Esc>", function()
-  require("util.runner").hide_terminal()
-end, { desc = "Hide terminal" })
-
 map("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find files" })
 map("n", "<leader>fg", function() Snacks.picker.grep() end, { desc = "Grep" })
 map("n", "<leader>fh", function() Snacks.picker.help() end, { desc = "Help tags" })
